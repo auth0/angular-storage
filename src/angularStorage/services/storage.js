813c2e0095c7d65c20dda7473e3b1edf8933b499
Ramin B. <i8ramin@gmail.com>
2014-10-22 11:09:45 -0400

    Fix for Safari in private browsing mode

--- src/angularStorage/services/storage.js
+++ src/angularStorage/services/storage.js
@@ -1,6 +1,24 @@
 angular.module('angular-storage.storage', [])
   .service('storage', function($window, $injector) {
-    if ($window.localStorage) {
+    var storeDisabled = false;
+    var testKey = '__storejs__';
+    
+    // Need to do try/catch because Safari in private
+    // browsing mode throws an exception when trying to
+    // call setItem
+    try {
+      $window.localStorage.setItem(testKey, testKey);
+
+      if ($window.localStorage.getItem(testKey) !== testKey) {
+        storeDisabled = true;
+      }
+
+      $window.localStorage.removeItem(testKey);
+    } catch(e) {
+      storeDisabled = true;
+    }
+    
+    if (!storeDisabled) {
       this.set = function(what, value) {
         return $window.localStorage.setItem(what, value);
       };
