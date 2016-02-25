fd9368b434a36d939809a91b500bf074685f771c
Ryan Chenkie <chenkie@users.noreply.github.com>
2016-02-25 16:52:42 -0700

    Updated ngCookies

--- src/angularStorage/services/cookieStorage.js
+++ src/angularStorage/services/cookieStorage.js
@@ -1,16 +1,15 @@
 angular.module('angular-storage.cookieStorage', [])
-  .service('cookieStorage', function ($injector) {
-    var $cookieStore = $injector.get('$cookieStore');
+  .service('cookieStorage', ["$cookies", function ($cookies) {
 
     this.set = function (what, value) {
-      return $cookieStore.put(what, value);
+      return $cookies.put(what, value);
     };
 
     this.get = function (what) {
-      return $cookieStore.get(what);
+      return $cookies.get(what);
     };
 
     this.remove = function (what) {
-      return $cookieStore.remove(what);
+      return $cookies.remove(what);
     };
   });
